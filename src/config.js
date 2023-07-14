287a419352565bd3cd27b49e43e2e1ce34bfa99e
ali-shan-technoverse <127727559+ali-shan-technoverse@users.noreply.github.com>
2023-07-14 10:02:48 +0500

    config changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
-export const baseApiUrl = "https://biomass-gcp-server-rnt37kunua-uc.a.run.app"
-// export const baseApiUrl = ""
-// export const baseApiUrl = "https://15f9-59-103-102-9.ngrok-free.app"
+export const baseApiUrl = ""
 export const rackStatusEndPoint = baseApiUrl + "/api/rack-status"
